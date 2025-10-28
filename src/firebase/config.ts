67c7076258df0f4dc9602610e2ef7941fa337f11
ONE IN ALL <irshadahmed00756@gmail.com>
2025-10-28 12:27:15 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-7186015998-fa4fe",
+  "appId": "1:905218028023:web:2e2cece641cf95549efef2",
+  "apiKey": "AIzaSyDmXRQ88_Xo7NgAEOYs-sbOUO73yIZe-Uo",
+  "authDomain": "studio-7186015998-fa4fe.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "905218028023"
+};
