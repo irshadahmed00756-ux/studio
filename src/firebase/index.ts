a1af1bf53dd5e5fafdd68691d1beae17f2b6d630
ONE IN ALL <irshadahmed00756@gmail.com>
2025-10-28 13:31:37 +0000

    // Import the functions you need from the SDKs you need import { initial

--- src/firebase/index.ts
+++ src/firebase/index.ts
@@ -4,6 +4,7 @@ import { firebaseConfig } from '@/firebase/config';
 import { initializeApp, getApps, getApp, FirebaseApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore'
+import { getAnalytics, isSupported } from 'firebase/analytics';
 
 // IMPORTANT: DO NOT MODIFY THIS FUNCTION
 export function initializeFirebase() {
@@ -33,10 +34,12 @@ export function initializeFirebase() {
 }
 
 export function getSdks(firebaseApp: FirebaseApp) {
+  const analytics = isSupported().then(yes => yes ? getAnalytics(firebaseApp) : null);
   return {
     firebaseApp,
     auth: getAuth(firebaseApp),
-    firestore: getFirestore(firebaseApp)
+    firestore: getFirestore(firebaseApp),
+    analytics,
   };
 }
 
